3b43c87d6d89d09dfef6f41f57df0c40078d25b8
syoung <syoung1394@naver.com>
2017-12-28 01:04:11 +0900

    google map API

---
+++ app/src/main/java/sinabro/reservation/ReservationActivity.java
@@ -0,0 +1,46 @@
+package sinabro.reservation;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ReservationActivity extends FragmentActivity implements OnMapReadyCallback {
+    // 구글 맵 참조변수 생성
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // SupportMapFragment을 통해 레이아웃에 만든 fragment의 ID를 참조하고 구글맵을 호출한다.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this); //getMapAsync must be called on the main thread.
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // 구글 맵 객체를 불러온다.
+        mMap = googleMap;
+
+        // 서울 여의도에 대한 위치 설정
+        LatLng seoul = new LatLng(37.52487, 126.92723);
+
+        // 구글 맵에 표시할 마커에 대한 옵션 설정
+        MarkerOptions makerOptions = new MarkerOptions();
+        makerOptions
+                .position(seoul)
+                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.");
+
+        // 마커를 생성한다.
+        mMap.addMarker(makerOptions);
+
+        //카메라를 여의도 위치로 옮긴다.
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+    }
+    }
